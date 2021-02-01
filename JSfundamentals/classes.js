8f9a6b2fd6e638e85151310b9df7ea231e7873aa
Robert Alestra <bobbyalestra135@gmail.com>
2021-02-01 11:40:35 -0500

    using classes in JS

--- JSfundamentals/classes.js
+++ JSfundamentals/classes.js
@@ -1,8 +1,16 @@
 
 
+// when having an object with more than one method create a "blue print";
 
 
 const person = {
+    name : 'Bobby',
+    walk() {
+        console.log(walk);
+    }
+};
+
+const person2 = {
     name : 'Bobby',
     walk() {
         console.log(walk);
