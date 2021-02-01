cd98b7fadbeaed0451ab6308715fa36cfa5acf6d
Robert Alestra <bobbyalestra135@gmail.com>
2021-02-01 11:47:49 -0500

    using inheritance

---
+++ JSfundamentals/inheritance.js
@@ -0,0 +1,17 @@
+
+
+// when having an object with more than one method create a "blue print to create objects of that type";
+
+class Person {
+    constructor(name) {
+        this.name = name
+    }
+    walk() {
+    console.log(walk);
+}
+}
+
+// to create an object using the class or "blueprint" you must define it as "new"
+// use the new Person (parameter) to add the new constructor
+const person = new Person('Bobby');
+person.walk
