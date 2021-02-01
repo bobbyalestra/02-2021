8c3601de3b4289f4d3ccbe6ce95d74072c833216
Robert Alestra <bobbyalestra135@gmail.com>
2021-02-01 11:33:39 -0500

    using the spread operator

--- JSfundamentals/spreadOp.js
+++ JSfundamentals/spreadOp.js
@@ -5,6 +5,8 @@
 const first = [1,2,3];
 const second= [4,5,6];
 
-const combined = first.concat(second);
+// const combined = first.concat(second);
+// const combined = [...first, ...second]
 
-const combined = [...first, ...second]
+const clone = [...first];
+console.log(first);
