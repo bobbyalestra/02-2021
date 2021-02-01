b2b7c03ddc3f1096a80ead213e13d1c745e69bcb
Robert Alestra <bobbyalestra135@gmail.com>
2021-02-01 11:35:51 -0500

    using the spread operator

--- JSfundamentals/spreadOp.js
+++ JSfundamentals/spreadOp.js
@@ -1,12 +1,16 @@
 
+// const first = [1,2,3];
+// const second= [4,5,6];
+
+// // const combined = first.concat(second);
+// // const combined = [...first, ...second]
+
+// const clone = [...first];
+// console.log(first);
 
 
+// able to spread objects
+const first = {name : 'Bobby'};
+const seconf = {job : "Programmer"};
 
-const first = [1,2,3];
-const second= [4,5,6];
-
-// const combined = first.concat(second);
-// const combined = [...first, ...second]
-
-const clone = [...first];
-console.log(first);
+const combined = {...first, ...second}
