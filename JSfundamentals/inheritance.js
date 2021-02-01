4eff75546aaf1453719b52c8c8f2b402fef72382
Robert Alestra <bobbyalestra135@gmail.com>
2021-02-01 13:20:11 -0500

    using inheritance

--- JSfundamentals/inheritance.js
+++ JSfundamentals/inheritance.js
@@ -13,5 +13,28 @@ class Person {
 
 // to create an object using the class or "blueprint" you must define it as "new"
 // use the new Person (parameter) to add the new constructor
-const person = new Person('Bobby');
-person.walk
+
+
+
+
+
+// new Teacher inherits the constructor from  Person
+class Teacher extends Person {
+
+    
+    constructor(name,degree){
+    // when adding a constructor to a child Class you have to call the super()
+    super(name);
+    this.degree = degree
+   
+    }
+    teach() {
+        console.log("teach")
+    }
+
+
+}
+// adding the (parameter) willo give it the constructor of name
+
+const teacher = new Teacher('Bobby', "Programmer");
+console.log(Teacher)
