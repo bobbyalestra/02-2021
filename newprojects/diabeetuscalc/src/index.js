368ad98708a374afb52beca602370007b7b7280c
Robert Alestra <bobbyalestra135@gmail.com>
2021-02-08 16:13:32 -0500

    fixing components directory

--- newprojects/diabeetuscalc/src/index.js
+++ newprojects/diabeetuscalc/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Forms from './components/Forms';
-import CarbCorrect from './components/CarbCorrect';
-import './components/CarbCorrect.css'
+import CarbCorrect from './components/CarbCorrect/CarbCorrect';
+
 
 
 
