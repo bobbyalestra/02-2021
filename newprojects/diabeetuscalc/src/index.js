c86e369638f67707699f7422282e24e5eadb2c61
Robert Alestra <bobbyalestra135@gmail.com>
2021-02-08 15:33:08 -0500

    update

--- newprojects/diabeetuscalc/src/index.js
+++ newprojects/diabeetuscalc/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Forms from './Forms';
+import Forms from './components/Forms';
 
 
 
