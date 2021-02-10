411dd9dff9f20a4792d586680c163d0fcaa7ee18
Robert Alestra <bobbyalestra135@gmail.com>
2021-02-10 15:20:55 -0500

    working with Python inputs

--- newprojects/python/zNFAL trail/main.py
+++ newprojects/python/zNFAL trail/main.py
@@ -1,6 +1,16 @@
 
 print("Welcome To NFAL Trail")
 name = input("What is your Name?")
-age = input("How Old Are You?")
+age = int(input("How Old Are You?"))
 
 print("Hello", name," you are", age, "years old")
+
+
+if age >= 18:
+    print("You are old enough")
+elif age >=24:
+    print("you are not old enought Need Parents Supervision")
+else:
+    print("you are not old enought")
+
+        
