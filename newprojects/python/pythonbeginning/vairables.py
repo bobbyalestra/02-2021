21abf29c810a2407facc7e8e7014d372dd71ac3f
Robert Alestra <bobbyalestra135@gmail.com>
2021-02-11 09:39:16 -0500

    more python porojects

---
+++ newprojects/python/pythonbeginning/vairables.py
@@ -0,0 +1,14 @@
+
+
+character_name = "Bobby"
+character_age = 32
+is_male = False
+print("There once was a nigga named " + character_name + ", ")
+print("He was the shit")
+print("That nigga was " + character_age + ", ")
+
+character_name ="Mike"
+character_age = 29
+print("There once was a nigga named " + character_name + ", ")
+print("He was the shit")
+print("That nigga was " + character_age + ", ")
