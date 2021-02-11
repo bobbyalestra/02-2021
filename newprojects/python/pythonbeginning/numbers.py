2c31227f2a8023aa7969acd95387bc707116c72b
Robert Alestra <bobbyalestra135@gmail.com>
2021-02-11 09:53:05 -0500

    python with numbers

--- newprojects/python/pythonbeginning/numbers.py
+++ newprojects/python/pythonbeginning/numbers.py
@@ -12,5 +12,15 @@ print(3 + 5)
 # you can specify the order of operations
 print(3 * (4+5))
 
-
+# the modulus operator show the remaining number after the division
 print(10 % 3)
+
+
+
+# how to add a num variable
+my_num = 5
+print(my_num)
+
+#convert number to string
+
+print(str(my_num))
