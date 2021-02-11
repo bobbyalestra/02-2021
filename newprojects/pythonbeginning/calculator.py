a450ab1c774c927dffe4f004d0e4177ceebe66e2
Bobby Alestra <bobbyalestra135@gmail.com>
2021-02-11 12:15:07 -0500

    adding floats in Python

--- newprojects/pythonbeginning/calculator.py
+++ newprojects/pythonbeginning/calculator.py
@@ -1,9 +1,16 @@
 
 
-
+# must add the int function to make it an interger else it will be a string
 
 number1 = input("Enter a number")
 number2 = input("Enter a second number")
 result = int(number1) + int(number2)
 
 print(result)
+
+
+num3 = input("Enter a number")
+num4 = input("Enter a second number)
+total = float(num3) + float(num4)
+
+print(total)
