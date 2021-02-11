9f288a66dadba2c6c0fc3748c09137193dac9e4f
Bobby Alestra <bobbyalestra135@gmail.com>
2021-02-11 18:48:27 -0500

    using arrays in python

---
+++ newprojects/pythonbeginning/lists.py
@@ -0,0 +1,1 @@
+family = [ "Bobby", "Sammy", "Aron" ]
---
+++ newprojects/pythonbeginning/madlibs.py
@@ -0,0 +1,10 @@
+
+
+color = input("Enter a color")
+pluralnoun = input("Enter A Plural Noun")
+person = input("Enter A Persons Name")
+
+
+print("Roses are" + color)
+print(pluralnoun + "are blue")
+print("i love " + person)
