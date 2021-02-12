fec4e7bde0b1ae50f0a680aa11fb84e1eea10237
Bobby Alestra <bobbyalestra135@gmail.com>
2021-02-11 21:09:36 -0500

    functions with parameters oin python

--- newprojects/python/functions.py
+++ newprojects/python/functions.py
@@ -6,9 +6,9 @@
 # to write functions use def
 # name of function () and : 
 # MUST ME TABBEED IN OR IT WILL NOT RUN!!
-def sayhi():
-    print("Hello!!!")
+def sayhi(name):
+    print("Hello!!!" + name)
 
-    print("Top")
-sayhi()
-    print("Botton")
+print("Top")
+sayhi("Bobby")
+print("Botton")
