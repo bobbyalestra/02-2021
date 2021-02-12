47360b374e0c313da172467bcb4ae9465cfeb3a5
Bobby Alestra <bobbyalestra135@gmail.com>
2021-02-12 14:22:04 -0500

    Python comparisons check whih number is greatest

--- newprojects/pythonbeginning/moreifsandcomparison.py
+++ newprojects/pythonbeginning/moreifsandcomparison.py
@@ -1,7 +1,17 @@
 
 
 
-
-
-
 # more work with if statements and comparisons
+
+# started off creatting th3 function max+num
+# gave it threem parameters
+# made an if statement to compare if num1 is greater than or equal to num2 ANDS num is greater than or equal to num3
+
+
+def max_num(num1, num2, num3):
+
+    #if all comparisons are true then num1 is largest
+    if num1 >= num2 and num1 >=num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
