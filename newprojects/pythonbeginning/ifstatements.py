b321da4ae7ef3e41382760a060b7dd53beee6b59
Bobby Alestra <bobbyalestra135@gmail.com>
2021-02-12 13:43:43 -0500

    working with if statements and ifel and else statements

--- newprojects/pythonbeginning/ifstatements.py
+++ newprojects/pythonbeginning/ifstatements.py
@@ -2,12 +2,16 @@
 
 
 # if statements ahve a certain structure
-
+# the or key word will allow either condirtions
+# and frequires both
 
 is_male = True
-is_tall = True
+is_tall = False
 
-if is_male or is_tall:
-    print("you are a male")
+if is_male and is_tall:
+    print("you are a male or tall or both")
+elif is_male and not(is_tall):
+    print(" you are a short male")
+elif not is_male and (is_tall):
 else:
-    print("not man ")
+    print("not man not tall ")
