3184f4873ba91804b9c2ce249c6674cc47bc42bb
Bobby Alestra <bobbyalestra135@gmail.com>
2021-02-12 13:14:32 -0500

    usiing the return function with number isn Python

--- newprojects/pythonbeginning/returnstatement.py
+++ newprojects/pythonbeginning/returnstatement.py
@@ -1,6 +1,13 @@
+#cubing a number
+
 def cube(num):
   return  num*num*num
 
 
-print(cube(3))
+# setting the variable result to the cube function with the arguement of 4 to result in 4 cubed
+result = cube(4)
+
+print(result)
+
+
 
