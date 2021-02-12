5d5d0f9d1e8e2ba49e1f751e11de4cbf49fbfd42
Bobby Alestra <bobbyalestra135@gmail.com>
2021-02-11 21:06:48 -0500

    functions/Library/Frameworks/Python.framework/Versions/3.9/bin/python3

--- newprojects/python/functions.py
+++ newprojects/python/functions.py
@@ -4,5 +4,11 @@
 
 
 # to write functions use def
-
+# name of function () and : 
+# MUST ME TABBEED IN OR IT WILL NOT RUN!!
 def sayhi():
+    print("Hello!!!")
+
+    print("Top")
+sayhi()
+    print("Botton")
