8538bc2289c0fdf45539f3a76f6fe5237795b579
Bobby Alestra <bobbyalestra135@gmail.com>
2021-02-11 19:07:40 -0500

    working on switch an index woth another

--- newprojects/pythonbeginning/lists.py
+++ newprojects/pythonbeginning/lists.py
@@ -7,3 +7,12 @@ print(family)
 # use bracket notation to find the oindex
 
 print(family[1])
+
+# adding a negative value to a number it will print the result backards that many
+
+print(family[1])
+
+
+# to gab an element and switch it
+
+print(family[1:"BabyGirl"])
