0dd245a7e01835cf5a4a2bba2928c4320eeddad8
Bobby Alestra <bobbyalestra135@gmail.com>
2021-02-13 10:55:39 -0500

    using for loops in Python

--- newprojects/pythonbeginning/forloops.py
+++ newprojects/pythonbeginning/forloops.py
@@ -4,8 +4,8 @@
 # used to loop over different collections of items
 # start with for
 # name of the collection
-# then use the keyword in what you want to go through ie string
-#
+# then use the keyword in what you want to go through ie string finish with a colon
+# name function 
 #
 #
 
