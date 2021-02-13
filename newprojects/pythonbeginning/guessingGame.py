c118a156d43072e0c76a3179aad019855a884722
Bobby Alestra <bobbyalestra135@gmail.com>
2021-02-13 09:55:48 -0500

    making a guessing game in Python using while loops

--- newprojects/pythonbeginning/guessingGame.py
+++ newprojects/pythonbeginning/guessingGame.py
@@ -5,10 +5,15 @@
 # make the guess initializer equal an input telling the user to enter their guess
 # will continue to run loop until you guess the secret word
 
+
+# set a guess variable
+# add  the guess count and it crement it once for each guess
 secret_word = "BabyGirl"
 guess = ""
+guess_count = 0
 
 
 while guess != secret_word:
     guess = input("Enter Guess : ")
+    guess_count += 1
 print("You Win")
