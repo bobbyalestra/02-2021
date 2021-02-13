2b04be4dfc027de2818d6b53973cbd56ca374fb5
Bobby Alestra <bobbyalestra135@gmail.com>
2021-02-13 11:05:44 -0500

    using for loops in Python with arrays and range

--- newprojects/pythonbeginning/forloops.py
+++ newprojects/pythonbeginning/forloops.py
@@ -6,8 +6,13 @@
 # name of the collection
 # then use the keyword in what you want to go through ie string finish with a colon
 # name function 
-#
-#
+
+
+
+# index in range() will print out each number second nuumber not included
+# can add two parameter 3, 10
+for index in range(3, 10):
+    print(index)
 
 family_members = ["Daddy", "Mummy", "Cameron", "Aron", "Litte Princess"]
 
@@ -21,9 +26,6 @@ for family in family_members :
     print(family)
 
 
+# adding the length function len(parameter) wll tell you how many charaters there are in the parameter
 
-
-# index in range() will print out each number not ten
-# can add two parameter 3, 10
-for index in range(3, 10):
-    print(index)
+print(len(family))
