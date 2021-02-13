cf17855997a6b04fa9966378d55eff8f02c8436a
Bobby Alestra <bobbyalestra135@gmail.com>
2021-02-13 11:22:41 -0500

    making an exponent function in Python

--- newprojects/pythonbeginning/buildingexponent.py
+++ newprojects/pythonbeginning/buildingexponent.py
@@ -11,7 +11,7 @@ print(2**3)
 # make a variable for the result
 #make a for loop
 # index in range of the number to blankth power
-# makong the index equal 1 each time it loops through the the base number will start times 1 equalling itself then continuing the loop until it reaches the power
+# making the index equal 1 each time it loops through the the base number will start times 1 equalling itself then continuing the loop until it reaches the power
 
 def raisingPower(base_num, power_num):
     result = 1
