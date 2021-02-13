7091dc80b1e5f625391a9eb535290561f5d62d22
Bobby Alestra <bobbyalestra135@gmail.com>
2021-02-13 09:51:51 -0500

    making a guessing game

--- newprojects/pythonbeginning/guessingGame.py
+++ newprojects/pythonbeginning/guessingGame.py
@@ -1,7 +1,8 @@
 
 
 # step one declare a variable for the secret word and an empty string to store your guesses
-#
+# create a while loop with the conditions of your guess not equal to the secret word
+# make the guess initializer equal an input telling the user to enter their guess
 
 
 secret_word = "BabyGirl"
