58230604c875703d5dc00e8cb4084a8887805a25
Bobby Alestra <bobbyalestra135@gmail.com>
2021-02-13 10:06:05 -0500

    making a guessing game in Python using while loops and if statements

--- newprojects/pythonbeginning/guessingGame.py
+++ newprojects/pythonbeginning/guessingGame.py
@@ -9,7 +9,7 @@
 # set a guess variable
 # add  the guess count and it crement it once for each guess
 # add a guess limit variable
-# create another varibale called out of guess...a boolean value so if its true the are out of guesses false equals ore chances
+# create another varibale called out of guess...a boolean value so if its true the are out of guesses false equals no more chances  Set it as a False defaul
 # add an if statement inside of the while loop to check first to see if they are out of chances by making the condition if guess count is less than count limit
 # check for proper indentation!
 # finish with an else statement t equal out of guesses = true
@@ -19,7 +19,7 @@ secret_word = "BabyGirl"
 guess = ""
 guess_count = 0
 guess_limit = 3
-out_of_guesses = True
+out_of_guesses = False
 
 
 
