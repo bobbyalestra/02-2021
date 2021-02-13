3d8fc0fe1ff00f93204a09a5fb7af0db0ee07c0f
Bobby Alestra <bobbyalestra135@gmail.com>
2021-02-13 11:55:35 -0500

    making a translator in Python

--- newprojects/pythonbeginning/2dlistsandnestedarrays.py
+++ newprojects/pythonbeginning/2dlistsandnestedarrays.py
@@ -18,7 +18,7 @@ print(number_grid[3][0])
 
 for row in number_grid:
  # prints out the list as its nested   
-    print(row)
+    #print(row)
 # will make each fist index into its own column
     for col in row:
         print(col)
---
+++ newprojects/pythonbeginning/translator.py
@@ -0,0 +1,3 @@
+
+
+# making a Python Translalor 
