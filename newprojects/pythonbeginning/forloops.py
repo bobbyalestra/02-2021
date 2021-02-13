b99850ae86e4c584c9c70180be57bf19c954a1b2
Bobby Alestra <bobbyalestra135@gmail.com>
2021-02-13 10:55:03 -0500

    using for loops in Python

--- newprojects/pythonbeginning/forloops.py
+++ newprojects/pythonbeginning/forloops.py
@@ -2,3 +2,13 @@
 
 
 # used to loop over different collections of items
+# start with for
+# name of the collection
+# then use the keyword in what you want to go through ie string
+#
+#
+#
+
+
+for letter in "BabyGirl" :
+    print(letter)
