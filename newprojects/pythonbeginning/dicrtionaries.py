2467eb52c7728a61d15b5fc327a90d062389a9f2
Bobby Alestra <bobbyalestra135@gmail.com>
2021-02-13 09:13:19 -0500

    making dictionaries in Python

---
+++ newprojects/pythonbeginning/dicrtionaries.py
@@ -0,0 +1,23 @@
+
+
+
+# 
+
+
+
+monthConversion = {
+
+    "Jan" : "January",
+    "Feb" : "February",
+    "Mar" : "March",
+    "Apr" : "April",
+    "May" : "May",
+    "Jun" : "June",
+    "Jul" : "July",
+    "Aug" : "August",
+    "Sept": "September",
+    "Oct" : "October",
+    "Nov" : "Novemeber",
+    "Dec" : "Decemeber"
+
+}
