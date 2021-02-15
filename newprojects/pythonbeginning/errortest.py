f7bb00615a085b7efa1a8464e756735cc2065728
Bobby Alestra <bobbyalestra135@gmail.com>
2021-02-15 11:58:28 -0500

    using try and except in Python

--- newprojects/pythonbeginning/errortest.py
+++ newprojects/pythonbeginning/errortest.py
@@ -5,5 +5,16 @@
 
 
 #trying to set the number variable to an integer input
+# like this it will throw an error if you enter a letter
 number = int(input("Enter A Number: "))
 print(number)
+
+
+
+# by adding the try and indenting the variable and function it 
+
+try:
+    numbers = int(input("Enter A Number: "))
+    print(numbers)
+except:
+    print("Invalid Input")
