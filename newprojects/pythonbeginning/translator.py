c0f6b2343a43350f727c4e615932bac79c9405f9
Bobby Alestra <bobbyalestra135@gmail.com>
2021-02-15 11:22:11 -0500

    making letter translator

--- newprojects/pythonbeginning/translator.py
+++ newprojects/pythonbeginning/translator.py
@@ -7,8 +7,11 @@
 def translate(phrase):
     translation = ""
     for letter in phrase:
-        if letter in "AEIOUaeiou":
-            translation = translation + "b"
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "B"
+            else:
+                translation = translation + "b"
         else:
             translation = translation + letter
     return translation
