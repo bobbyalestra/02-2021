e7dacf85559cea86120ae8fd738053b6eeae8920
Bobby Alestra <bobbyalestra135@gmail.com>
2021-02-15 11:10:34 -0500

    changing letters in string in Python

--- newprojects/pythonbeginning/translator.py
+++ newprojects/pythonbeginning/translator.py
@@ -8,8 +8,8 @@ def translate(phrase):
     translation = ""
     for letter in phrase:
         if letter in "AEIOUaeiou":
-            translation = translation + "B"
+            translation = translation + "b"
         else:
-            translation =  translation + letter
-        return translate
-print(translate(input("Enter A Phrase")))
+            translation = translation + letter
+    return translation
+print(translate(input("Enter A Phrase ")))
