b40b775e7a3bb1ecb9a3f5c8b25a94a2de52aa81
Bobby Alestra <bobbyalestra135@gmail.com>
2021-02-15 10:53:15 -0500

    making letter translator

--- newprojects/pythonbeginning/translator.py
+++ newprojects/pythonbeginning/translator.py
@@ -2,3 +2,13 @@
 
 # making a Python Translalor 
 # making all vowels equal a certain letter
+
+
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter in "AEIOUaeiou":
+            translation = translation + "B"
+        else:
+            translation =  translattion + letter
+        return translate
