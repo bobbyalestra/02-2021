76d3886795efa0b0f46523ecfcfb0b3566269b25
Bobby Alestra <bobbyalestra135@gmail.com>
2021-02-16 11:13:09 -0500

    appending data in Python

--- newprojects/pythonbeginning/appendinddata.py
+++ newprojects/pythonbeginning/appendinddata.py
@@ -3,6 +3,22 @@
 
 # change the mode to a
 
+# employee_file = open("employees.txt", "a" )
+
+
+# # \n so you can get it on a new line
+# employee_file.write("\nSammy - Customer Service")
+
+
+
+
+
+# employee_file.close()
+
+
+
+# using the write function will only add what you put in
+
 employee_file = open("employees.txt", "a" )
 
 
