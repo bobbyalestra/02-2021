8292714972f7dcaa11fccdbbad203984a1d23ffa
Bobby Alestra <bobbyalestra135@gmail.com>
2021-02-27 18:27:38 -0500

    working on adding MongoDB to social media app

--- social-media-app/index.js
+++ social-media-app/index.js
@@ -2,6 +2,8 @@ const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
+const { MONGODB } = require('config.js')
+
 // create a type degfs to long the contents of the Query you are trying to restore
 // make it a const variable ann do as follows for which type, the ! makes its it a requirement
 const typeDefs = gql`
