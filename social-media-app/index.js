5da13a651243b1470e046e2b325ade9cbec4bab9
Bobby Alestra <bobbyalestra135@gmail.com>
2021-03-01 12:54:38 -0500

    wirking with mongoDB to add my Query to a database

--- social-media-app/index.js
+++ social-media-app/index.js
@@ -2,7 +2,7 @@ const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
-const { MONGODB } = require('config.js')
+const { MONGODB } = require('./config.js')
 
 // create a type degfs to long the contents of the Query you are trying to restore
 // make it a const variable ann do as follows for which type, the ! makes its it a requirement
@@ -31,17 +31,18 @@ const server = new ApolloServer({
 
 // next we need to connect to mongoDB using mongoose.connect("Connection String Here") and get the cpnnection string
 
-mongoose.connect()
+mongoose.connect(MONGODB, { useUnifiedTopology: true,
+    useNewUrlParser: true,})
+    .then(() => {
+            console.log('MongoDB Coneccted Kidddd')
+        return server.listen({ port: 5000})
+    })
 
-
-
-server.listen({port: 5000})
 // that requires a promise to we use .then
 // res means a result object
 
 
 .then(res => {
     console.log(`server running at ${res.url}`)
-})
-
+});
 ///must use the CMD node index to restart the server and its update
