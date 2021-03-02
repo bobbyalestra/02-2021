7afa42cab3726e1a445fe7d2986e602a5356ed84
Bobby Alestra <bobbyalestra135@gmail.com>
2021-03-02 14:33:32 -0500

    creating a new user for social media app

--- social-media-app/graphQl/resolvers/users.js
+++ social-media-app/graphQl/resolvers/users.js
@@ -1,10 +1,33 @@
 
 // create a constant for the user and have it require the User file for mutations
+
+
+const bcrypt = require('bcrypths')
+const jwt = require('jsonwebtoken');
+const User = require('../../models/User');
 const user = require('../../models/User');
 
 
 module.exports = {
     Mutation: {
-        register(parent, args, context, info)
+        async register(_, 
+            {
+                registerInput : {username, email, password, comfirmPassword} 
+        },
+         context, 
+         info)
+         
+         {
+           //tTODO validate user data
+           // TODO Makes sure user doesnt alreaxy exist
+           //hash password and create auth token 
+           password = await bcrypt.hash(password, 12)
+
+           const newUser = new User({
+               email,
+               username,
+               password
+           }
+        }
     }
 }
